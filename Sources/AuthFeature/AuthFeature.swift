8d7ad30828013daedce9b3a7e25a6623b463d1f5
Yehor Myropoltsev <egormiropoltsev79@gmail.com>
2024-11-11 02:30:32 +0200

    chore(auth): move crypto function to swift helpers

--- Sources/AuthFeature/AuthFeature.swift
+++ Sources/AuthFeature/AuthFeature.swift
@@ -270,22 +270,3 @@ extension Error {
         return false
     }
 }
-
-private func randomNonceString(length: Int = 32) -> String {
-    precondition(length > 0)
-
-    var randomBytes = [UInt8](repeating: 0, count: length)
-    guard SecRandomCopyBytes(kSecRandomDefault, randomBytes.count, &randomBytes) == errSecSuccess else {
-        fatalError("Unable to generate nonce. SecRandomCopyBytes failed.")
-    }
-
-    let charset = Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
-    return String(randomBytes.map { charset[Int($0) % charset.count] })
-}
-
-@available(iOS 13.0, *)
-private func sha256(_ input: String) -> String {
-    let inputData = Data(input.utf8)
-    let hash = SHA256.hash(data: inputData)
-    return hash.map { String(format: "%02x", $0) }.joined()
-}
