ff3bfb73d8353487aad0451dcd3b9eb37896e717
Yehor Myropoltsev <egormiropoltsev79@gmail.com>
2024-11-11 02:15:06 +0200

    fix(auth): refactor nonce generating functions

--- Sources/AuthFeature/AuthFeature.swift
+++ Sources/AuthFeature/AuthFeature.swift
@@ -273,32 +273,19 @@ extension Error {
 
 private func randomNonceString(length: Int = 32) -> String {
     precondition(length > 0)
+
     var randomBytes = [UInt8](repeating: 0, count: length)
-    let errorCode = SecRandomCopyBytes(kSecRandomDefault, randomBytes.count, &randomBytes)
-    if errorCode != errSecSuccess {
-        fatalError(
-            "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
-        )
+    guard SecRandomCopyBytes(kSecRandomDefault, randomBytes.count, &randomBytes) == errSecSuccess else {
+        fatalError("Unable to generate nonce. SecRandomCopyBytes failed.")
     }
 
-    let charset: [Character] =
-        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
-
-    let nonce = randomBytes.map { byte in
-        // Pick a random character from the set, wrapping around if needed.
-        charset[Int(byte) % charset.count]
-    }
-
-    return String(nonce)
+    let charset = Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+    return String(randomBytes.map { charset[Int($0) % charset.count] })
 }
 
-@available(iOS 13, *)
+@available(iOS 13.0, *)
 private func sha256(_ input: String) -> String {
     let inputData = Data(input.utf8)
-    let hashedData = SHA256.hash(data: inputData)
-    let hashString = hashedData.compactMap {
-        String(format: "%02x", $0)
-    }.joined()
-
-    return hashString
+    let hash = SHA256.hash(data: inputData)
+    return hash.map { String(format: "%02x", $0) }.joined()
 }
--- Sources/SupabaseSwiftClient/SupabaseSwiftClient+Live.swift
+++ Sources/SupabaseSwiftClient/SupabaseSwiftClient+Live.swift
@@ -20,6 +20,7 @@ extension SupabaseAuthError: LocalizedError {
 extension SupabaseSwiftClient: DependencyKey {
     public static var liveValue: SupabaseSwiftClient {
         let supabase = SupabaseClient(
+            // swiftlint:disable:next force_unwrapping
             supabaseURL: URL(string: "https://poekkltopyoklknjhfos.supabase.co")!,
             supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvZWtrbHRvcHlva2xrbmpoZm9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA2NTIzNTUsImV4cCI6MjA0NjIyODM1NX0.pYob-SjrNnrM_Wm44JQP2lSkOveHpqbowW8jRxIL5Zs"
         )
